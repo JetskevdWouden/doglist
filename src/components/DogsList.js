5a2b691e9348faa7d882320fc67b8a53f989984f
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-13 13:53:17 +0200

    Dogs List

---
+++ src/components/DogsList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+class DogsList extends Component {
+
+    renderDogBreed(breed) {
+        return <li key={breed}>{breed}</li>
+      }
+
+    render() {
+        const {dogBreeds} = this.props
+        return(
+            <div className="dogs-list">
+                <h1>Dogs List</h1>
+                {!dogBreeds && 'Loading...'}
+                {
+                    dogBreeds &&
+                    <ul>
+                        {dogBreeds.map(this.renderDogBreed)}
+                    </ul>
+                }
+            </div>
+        )
+    }
+}
+
+export default DogsList;
---
+++ src/components/DogsListContainer.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import superagent from  'superagent'
+import DogsList from './DogsList'
+
+class DogsListContainer extends Component {
+    state = {
+        dogBreeds: null
+    }
+
+    componentDidMount() {
+        superagent
+            .get('https://dog.ceo/api/breeds/list/all')
+            .then(response => {
+                const breeds = Object.keys(response.body.message)
+                this.updateBreeds(breeds)
+            })
+            .catch(console.error)
+    }
+
+    updateBreeds = (breeds) => {
+        this.setState({
+            dogBreeds: breeds
+        })
+    }
+
+    render() {
+        return (
+            <DogsList dogBreeds = {this.state.dogBreeds}/>
+        )
+    }
+}
+
+export default DogsListContainer
